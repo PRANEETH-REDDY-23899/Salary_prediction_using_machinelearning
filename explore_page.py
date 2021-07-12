b58da32f3756f23cf8813a42954004c0b97be8ea
praneeth reddy <nakkapraneeth23899@gmail.com>
2021-07-13 00:24:34 +0530

    first commit

---
+++ explore_page.py
@@ -0,0 +1,6 @@
+
+import streamlit as st
+
+def show_explore_page():
+    st.title("Explore Software Engineer Salaries")
+    st.write("check out this [link](https://www.kaggle.com/nakkapraneethreddy/salary-prediciton-using-machinelearning)")
